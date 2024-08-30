15a0462d86d007f942207015de65385f6a8889ac
AvdylDemukajj <avdyldemukaj38@gmail.com>
2024-08-31 00:07:58 +0200

    "adding webcam"

--- min.py
+++ min.py
@@ -10,6 +10,19 @@ class App:
         self.login_button_main_window = util.get_button(self.main_window, "Login", "green", self.login)
         self.register_new_user_button_main_window = util.get_button(self.main_window, "Register new user", "gray", self.register_new_user, fg='black')
 
+        self.login_button_main_window.place(x=750, y=300)
+        self.register_new_user_button_main_window.place(x=750, y=400)
+
+        self.webcam_label = util.get_img_label(self.main_window)
+        self.webcam_label.place(x=10, y=0, width=700, height=500)
+
+    def login(self):
+        pass
+
+    def register_new_user(self):
+        pass
+
+
     def start(self):
         self.main_window.mainloop()
 
