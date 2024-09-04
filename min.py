99a2455722f40810977100b1b1f9b80bdc05f766
AvdylDemukajj <avdyldemukaj38@gmail.com>
2024-09-04 22:50:08 +0200

    "logout function"

--- min.py
+++ min.py
@@ -16,10 +16,14 @@ class App:
         self.main_window = tk.Tk()
         self.main_window.geometry("1200x520+350+100")
 
-        self.login_button_main_window = util.get_button(self.main_window, "Login", "green", self.login)
-        self.register_new_user_button_main_window = util.get_button(self.main_window, "Register new user", "gray", self.register_new_user, fg='black')
+        self.login_button_main_window = util.get_button(self.main_window, 'login', 'green', self.login)
+        self.login_button_main_window.place(x=750, y=200)
 
-        self.login_button_main_window.place(x=750, y=300)
+        self.logout_button_main_window = util.get_button(self.main_window, 'logout', 'red', self.logout)
+        self.logout_button_main_window.place(x=750, y=300)
+
+        self.register_new_user_button_main_window = util.get_button(self.main_window, 'register new user', 'gray',
+        self.register_new_user, fg='black')
         self.register_new_user_button_main_window.place(x=750, y=400)
 
         self.webcam_label = util.get_img_label(self.main_window)
@@ -30,28 +34,39 @@ class App:
         self.db_dir = './db'
         if not os.path.exists(self.db_dir):
             os.mkdir(self.db_dir)
-        
+
         self.log_path = './log.txt'
 
     def add_webcam(self, label):
         if 'cap' not in self.__dict__:
-            self.cap = cv2.VideoCapture(0)
+            self.cap = cv2.VideoCapture(2)
 
         self._label = label
         self.process_webcam()
 
     def process_webcam(self):
         ret, frame = self.cap.read()
-        self.most_recent_capture_arr = frame
 
+        self.most_recent_capture_arr = frame
         img_ = cv2.cvtColor(self.most_recent_capture_arr, cv2.COLOR_BGR2RGB)
         self.most_recent_capture_pil = Image.fromarray(img_)
-
-        imgtk = ImageTk.PhotoImage(image = self.most_recent_capture_pil)
-        self._label.imgtk= imgtk
+        imgtk = ImageTk.PhotoImage(image=self.most_recent_capture_pil)
+        self._label.imgtk = imgtk
         self._label.configure(image=imgtk)
 
         self._label.after(20, self.process_webcam)
+
+    
+    def logout(self):
+        name = util.recognize(self.most_recent_capture_arr, self.db_dir)
+
+        if name in ['unknown_person', 'no_persons_found']:
+            util.msg_box('Ups...', 'Unknown user. Please register new user or try again.')
+        else:
+            util.msg_box('Hasta la vista !', 'Goodbye, {}.'.format(name))
+            with open(self.log_path, 'a') as f:
+                f.write('{},{},out\n'.format(name, datetime.datetime.now()))
+                f.close()
 
     def login(self):
         name = util.recognize(self.most_recent_capture_arr, self.db_dir)
@@ -68,22 +83,22 @@ class App:
         self.register_new_user_window = tk.Toplevel(self.main_window)
         self.register_new_user_window.geometry("1200x520+370+120")
 
-        self.accept_button_new_register_window = util.get_button(self.register_new_user_window, "Accept", "green", self.accept_register_new_user)
-        self.accept_button_new_register_window.place(x=750, y=300)
+        self.accept_button_register_new_user_window = util.get_button(self.register_new_user_window, 'Accept', 'green', self.accept_register_new_user)
+        self.accept_button_register_new_user_window.place(x=750, y=300)
 
-        self.try_again_button_new_register_window = util.get_button(self.register_new_user_window, "Try Again", "red", self.try_again_register_new_user)
-        self.try_again_button_new_register_window.place(x=750, y=400)
+        self.try_again_button_register_new_user_window = util.get_button(self.register_new_user_window, 'Try again', 'red', self.try_again_register_new_user)
+        self.try_again_button_register_new_user_window.place(x=750, y=400)
 
-        self.webcam_label = util.get_img_label(self.register_new_user_window)
-        self.webcam_label.place(x=10, y=0, width=700, height=500)
+        self.capture_label = util.get_img_label(self.register_new_user_window)
+        self.capture_label.place(x=10, y=0, width=700, height=500)
 
-        self.add_img_label(self.capture_label)
+        self.add_img_to_label(self.capture_label)
 
         self.entry_text_register_new_user = util.get_entry_text(self.register_new_user_window)
         self.entry_text_register_new_user.place(x=750, y=150)
 
-        self.text_label_register_nes_user = util.get_text_label(self.register_new_user_window,'Please, enter your username')
-        self.text_label_register_nes_user.place(x=750, y=70)
+        self.text_label_register_new_user = util.get_text_label(self.register_new_user_window, 'Please, \ninput username:')
+        self.text_label_register_new_user.place(x=750, y=70)
 
 
     def add_img_label(self, label):
